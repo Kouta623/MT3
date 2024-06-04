b55c7cb4360ae8a2fc21f6e74ce731ab01469807
Kouta623 <k023g1021@g.neec.ac.jp>
2024-06-04 13:10:52 +0900

    01-02途中

--- main.cpp
+++ main.cpp
@@ -16,22 +16,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	//クロス積確認用	
-	Vector3 v1{ 1.2f,-3.9f,2.5f };
-	Vector3 v2{ 2.8f,0.4f,-1.3f };
-	Vector3 cross = Cross(v1, v2);
 
-	Vector3 rotate{};
-	Vector3 translate{};
-
-	Vector3 kLocalvertical[3] =
-	{
-		0.0f,0.5f,0.0f,
-		-0.5f,-0.5f,0.0f,
-		0.5f,-0.5f,0.0f
-	};
-
-	Vector3 Scale = { 1.0f,1.0f,1.0f };
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -44,32 +29,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		rotate.y+=0.5f;
-		Matrix4x4 worldMatrix = MakeAffineMatrix(Scale, rotate, translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix(Scale, { 0.0f,0.0f,0.0f }, { 0.0f,0.0f,-5.0f });
-		Matrix4x4 viewMatrix = Invers(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePersectiveFovMatrix(0.45f, float(kWindowWith) / float(kWindowHigat), 0.1f, 100.0f);
-		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWith), float(kWindowHigat), 0.0f, 1.0f);
-		Vector3 screeVertices[3];
-		for (uint32_t i = 0; i < 3; ++i) {
-			Vector3 ndcVertex = Transform(kLocalvertical[i], worldViewProjectionMatrix);
-			screeVertices[i] = Transform(ndcVertex, viewportMatrix);
-		}
-
-
-		if (preKeys[DIK_W]) {
-			Scale.z-=0.1f;
-		}
-		if (preKeys[DIK_A]) {
-			translate.x-= 0.1f;
-		}
-		if (preKeys[DIK_S]) {
-			Scale.z+= 0.1f;
-		}
-		if (preKeys[DIK_D]) {
-			translate.x+= 0.1f;
-		}
+	
 		
 
 		///
@@ -82,10 +42,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		Novice::DrawTriangle(int(screeVertices[0].x), int(screeVertices[0].y), int(screeVertices[1].x), int(screeVertices[1].y), int(screeVertices[2].x), int(screeVertices[2].y), RED, kFillModeSolid);
-
 		
-		VectorScreenPrintf(0, 0, cross, "Cross");
 
 		///
 		/// ↑描画処理ここまで
--- mt3.h
+++ mt3.h
@@ -546,3 +546,4 @@ Vector3 Cross(const Vector3& v1, const Vector3& v2) {
 	return result;
 
 }
+
